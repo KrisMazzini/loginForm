6cefe31c2492b1f754cec1ecd1e59a4b022a8537
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-08-30 22:43:22 -0300

    add submit button

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -24,29 +24,69 @@ export function LoginForm() {
     ]
     
     return (
-        <Form>
-            <fieldset name="login-data">
-                {
-                    inputs.map(input => (
-                        <Input
+        <Container>
+            <form id="login-form">
+                <fieldset name="login-data">
+                    {
+                        inputs.map(input => (
+                            <Input
                             key={input.id}
                             icon={input.icon}
                             type={input.type}
                             id={input.id}
                             label={input.label}
                             placeholder={input.placeholder}
-                        />
-                    ))
-                }
+                            />
+                            ))
+                        }
+                </fieldset>
+            </form>
+            <fieldset form="login-form" name="submit">
+                <input type="submit" value="entrar" />
             </fieldset>
-        </Form>
+        </Container>
     )
 }
 
-const Form = styled.form`
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
     fieldset {
         display: flex;
         flex-direction: column;
         gap: 20px;
+    }
+
+    fieldset[name="submit"] > input {
+        width: 100%;
+        height: 51px;
+
+        border-radius: 4px;
+
+        border: none;
+        outline: none;
+
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 27px;
+        letter-spacing: 0.015em;
+
+        text-transform: uppercase;
+
+        cursor: pointer;
+
+        color: var(--submit-text);
+        background-color: var(--primary-color);
+
+        :hover {
+            filter: brightness(1.1);
+        }
+
+        :active {
+            filter: brightness(0.9);
+        }
     }
 `
